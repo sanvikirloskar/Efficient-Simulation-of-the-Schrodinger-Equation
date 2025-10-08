3d95610a5390309e219f5aab7631238fb82d422d
hannahanlac <hannahjohnstone47@gmail.com>
2025-10-08 17:13:34 +0100

    fixed my hamiltonian indexing, changed the matrix solver to scipy so now it works

--- Optical_Lattice_Hamiltonian.py
+++ Optical_Lattice_Hamiltonian.py
@@ -1,42 +1,46 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy as sp
 #parameters
 alpha = 10
-N = 3
+N = 10
 m = 1
 dims = 2*N + 1
 hbar = 1
-k_l = 2 * np.pi / 829
+k_l = 1
 
 def H_builder(alpha, N, q, hbar, k_l, m):
     E_r = (hbar**2)*(k_l**2)/(2*m)
     V_0 = alpha*E_r
     main_diag = np.zeros(dims)
-    for i in range(-N,N):
-        main_diag[i] = (((2*i+q)/hbar*k_l)**2*E_r)
+    for i in range(-N,N+1):
+        main_diag[i+N] = (((2*i+q)/hbar*k_l)**2*E_r)
     offset_diag = np.full(dims-1, V_0/4)
     H = np.zeros((dims,dims), dtype= float)
     np.fill_diagonal(H, main_diag)
     np.fill_diagonal(H[1:], offset_diag)
     np.fill_diagonal(H[:,1:], offset_diag)
-    return H
+    return H, main_diag, offset_diag
 
-q_vals = np.linspace(-hbar*k_l, hbar*k_l, 200)
+q_vals = np.linspace(-hbar*k_l, hbar*k_l, 500)
 energy_bands = np.zeros((len(q_vals), 2*N+1))
 
 for i, q in enumerate(q_vals):
-    H = H_builder(alpha, N, q, hbar, k_l, m)
-    eigenvalues = np.linalg.eigh(H)[0]
+    H, main_diag, offset_diag = H_builder(alpha, N, q, hbar, k_l, m)
+    eigenvalues = sp.linalg.eigh_tridiagonal(main_diag, offset_diag)[0]
     energy_bands[i, :] = eigenvalues  
 
 print(energy_bands)
 #Plotting the energy bands
-for band in range(2 * N + 1):
-    plt.plot(q_vals, energy_bands[:, band], label=f'Band {band + 1}')
+lines = []
+for band in range(5):
+    line, = plt.plot(q_vals, energy_bands[:, band], label=f'Band {band + 1}')
+    lines.append(line)
 
-plt.xlabel('q (Momentum)')
+plt.xlabel('q Value ($\hbar k$)')
 plt.ylabel('Energy')
-plt.title('Energy Bands in the First Brillouin Zone')
-plt.legend()
-plt.show()
 
+# Create a custom legend showing band labels with their respective colors
+plt.legend(handles=lines, labels=[f'Band {i+1}' for i, line in enumerate(lines)])
+
+plt.show()
